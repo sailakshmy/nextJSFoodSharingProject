a8507d115f2094f170c65949e583c07dda1a881e
sailakshmy <sailakshmy94@gmail.com>
2024-11-19 07:35:38 +0400

    Replaced img tag with Next's Image component

--- food-sharing-project/components/main-header.js
+++ food-sharing-project/components/main-header.js
@@ -1,11 +1,12 @@
 import logoImg from "@/assets/logo.png";
+import Image from "next/image";
 import Link from "next/link";
 import classes from "./main-header.module.css";
 const MainHeader = () => {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
 
