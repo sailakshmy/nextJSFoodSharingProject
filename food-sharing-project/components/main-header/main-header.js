19ba9cd07267a7d56ffd7c0adbd3f498800b7821
sailakshmy <sailakshmy94@gmail.com>
2024-11-20 07:18:45 +0400

    Updated the main Header component

---
+++ food-sharing-project/components/main-header/main-header.js
@@ -0,0 +1,32 @@
+import logoImg from "@/assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import MainHeaderBackground from "./main-header-background";
+import classes from "./main-header.module.css";
+
+const MainHeader = () => {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default MainHeader;
