24958dda8f28698c58c990373555b585c90a0be5
sailakshmy <sailakshmy94@gmail.com>
2024-11-27 07:08:02 +0400

    Added the loading file and related styles

---
+++ food-sharing-project/app/meals/loading.js
@@ -0,0 +1,6 @@
+import classes from "./loading.module.css";
+const MealsLoadingPage = () => {
+  return <p className={classes.loading}>Fetching meals.....</p>;
+};
+
+export default MealsLoadingPage;
