37a211df371cd39be36094544d06e6f9bd0d3051
sailakshmy <sailakshmy94@gmail.com>
2024-11-27 07:11:36 +0400

    Added Suspense component inside Meals page

--- food-sharing-project/app/meals/page.js
+++ food-sharing-project/app/meals/page.js
@@ -1,9 +1,15 @@
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 import Link from "next/link";
+import { Suspense } from "react";
 import classes from "./page.module.css";
-const MealsPage = ({ params }) => {
-  const meals = getMeals();
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+const MealsPage = async ({ params }) => {
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +25,11 @@ const MealsPage = ({ params }) => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals.....</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
