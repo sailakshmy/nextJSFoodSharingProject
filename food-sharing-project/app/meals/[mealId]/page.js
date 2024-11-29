b9d4d785a1dad0c794f54d84fb9c9b3a2c508755
sailakshmy <sailakshmy94@gmail.com>
2024-11-29 07:26:00 +0400

    Created the meal details page with styles

--- food-sharing-project/app/meals/[mealId]/page.js
+++ food-sharing-project/app/meals/[mealId]/page.js
@@ -1,6 +1,34 @@
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+import classes from "./page.module.css";
 const MealDetailsPage = async ({ params }) => {
   const { mealId } = await params;
-  return <h1>This is for {mealId}</h1>;
+  const meal = getMeal(mealId);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 };
 
 export default MealDetailsPage;
