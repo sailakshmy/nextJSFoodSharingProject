4b733bfa3b310675ef11f34f24eb61b683e22cdf
sailakshmy <sailakshmy94@gmail.com>
2024-12-10 07:17:45 +0400

    Added error page and server side input validation for share meal form

--- food-sharing-project/server-actions/shareMealServerAction.js
+++ food-sharing-project/server-actions/shareMealServerAction.js
@@ -3,6 +3,10 @@
 import { saveMeal } from "@/lib/meals";
 import { redirect } from "next/navigation";
 
+function isInvalidText() {
+  return !text || text.trim() === "";
+}
+
 export async function shareMeal(formData) {
   const meal = {
     title: formData.get("title"),
@@ -12,6 +16,18 @@ export async function shareMeal(formData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid input");
+  }
   await saveMeal(meal);
   redirect("/meals");
   // console.log("meal", meal);
