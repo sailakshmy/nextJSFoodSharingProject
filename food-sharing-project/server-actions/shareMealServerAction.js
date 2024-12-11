874e853d18685cadf99b55679612d751091a7533
sailakshmy <sailakshmy94@gmail.com>
2024-12-11 07:24:45 +0400

    Used the useActionState hook

--- food-sharing-project/server-actions/shareMealServerAction.js
+++ food-sharing-project/server-actions/shareMealServerAction.js
@@ -3,11 +3,11 @@
 import { saveMeal } from "@/lib/meals";
 import { redirect } from "next/navigation";
 
-function isInvalidText() {
+function isInvalidText(text) {
   return !text || text.trim() === "";
 }
 
-export async function shareMeal(formData) {
+export async function shareMeal(prevState, formData) {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -26,7 +26,8 @@ export async function shareMeal(formData) {
     !meal.image ||
     meal.image.size === 0
   ) {
-    throw new Error("Invalid input");
+    // throw new Error("Invalid input");
+    return { message: "Invalid input" };
   }
   await saveMeal(meal);
   redirect("/meals");
